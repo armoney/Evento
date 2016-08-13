04a2726ef58c177f16452843d9c4f3256b4bbf29
Armon Arcuri <email": null>
2016-08-13 15:50:16 -0700

    Twilio API inital commit

---
+++ twilio.rb
@@ -0,0 +1,13 @@
+require 'twilio-ruby'
+require 'sinatra'
+# p ENV[TWILLIO_ACCOUNT_SID]
+
+post '/receive_sms' do
+  content_type 'text/xml'
+
+  response = Twilio::TwiML::Response.new do |r|
+    r.Message 'Hey thanks for messaging me!'
+  end
+
+  response.to_xml
+end
