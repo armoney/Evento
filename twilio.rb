44f9ac6082329fc418a45a2a6c46ed4aa5df5c99
Armon Arcuri <email": null>
2016-08-13 18:49:25 -0700

    Twilio text to phone numbers

--- twilio.rb
+++ twilio.rb
@@ -1,13 +1,29 @@
+require 'dotenv'
+Dotenv.load
+
+require 'rubygems'
 require 'twilio-ruby'
-require 'sinatra'
-# p ENV[TWILLIO_ACCOUNT_SID]
 
-post '/receive_sms' do
-  content_type 'text/xml'
+account_sid = ENV['TWILIO_ACCOUNT_SID']
+auth_token = ENV['TWILIO_AUTH_TOKEN']
 
-  response = Twilio::TwiML::Response.new do |r|
-    r.Message 'Hey thanks for messaging me!'
-  end
+@client = Twilio::REST::Client.new account_sid, auth_token
 
-  response.to_xml
-end
+
+
+@client.account.messages.create({
+  :from => '+16502156875',
+  :to => '330xxx0939',
+  # :to => '206xxx0191',
+  :body => 'Hello world'
+})
+
+# post '/receive_sms' do
+#   content_type 'text/xml'
+
+#   response = Twilio::TwiML::Response.new do |r|
+#     r.Message 'Hey thanks for messaging me!'
+#   end
+
+#   response.to_xml
+# end
