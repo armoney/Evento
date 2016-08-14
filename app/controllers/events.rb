af98ba2f551df8528d223f6e379080c2f0c680c5
Armon Arcuri <email": null>
2016-08-13 23:42:22 -0700

    Add delete and edit routes for events

--- app/controllers/events.rb
+++ app/controllers/events.rb
@@ -1,8 +1,17 @@
+# Events NEW
 get '/users/:user_id/events/new' do
   @user = User.find(params[:user_id])
   erb :'/events/new'
 end
 
+# Events SHOW
+get '/users/:user_id/events/:id' do
+  @user = User.find(params[:user_id])
+  @event = Event.find(params[:id])
+  erb :'events/show'
+end
+
+# Events CREATE
 post '/users/:user_id/event' do
   @user = User.find(params[:user_id])
 
@@ -14,4 +23,25 @@ post '/users/:user_id/event' do
   Event.create(params[:event])
 
   redirect "/users/#{@user.id}"
+end
+
+# Events DELETE
+delete '/users/:user_id/events/:id' do
+  @event = Event.find(params[:id])
+  @event.destroy
+  redirect "/users/#{params[:user_id]}"
+end
+
+# Users EDIT
+get '/users/:user_id/events/:id/edit' do
+  @user = User.find(params[:user_id])
+  @event = Event.find(params[:id])
+  erb :'events/edit'
+end
+
+# Users UPDATE
+put '/users/:user_id/events/:id' do
+  @event = Event.find(params[:id])
+  @event.update(params[:event])
+  redirect "/users/#{params[:user_id]}/events/#{params[:id]}"
 end
