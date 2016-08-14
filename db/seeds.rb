f983780f98e345aec27d00bd9295b3e26de5dca1
Armon Arcuri <email": null>
2016-08-14 15:03:51 -0700

    Start seeds file

---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+# User.create {[
+#   :first_name => 'Armon',
+#   last_name: 'Arcuri',
+#   email: 'armon@armon.com',
+#   password_hash: '123'
+# ]}
+
+# Event.create {[
+#   title: 'My Super Party!',
+#   description: 'Hey everybody, come to my party!',
+#   address: '633 Folsom St., San Francisco',
+#   date: nil,
+#   time: nil,
+#   user_id: 1
+# ]}
