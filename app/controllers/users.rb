0af52290801bd9e72503f619c7029be8d120f21b
Armon Arcuri <email": null>
2016-08-13 20:08:41 -0700

    Fix post route bug in users

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -12,7 +12,7 @@ post '/users' do
     @user = User.new(params[:user])
     if @user.save
       session[:id] = @user.id
-      redirect '/users/#{@user.id}'
+      redirect "/users/#{@user.id}"
     else
       @errors = @user.errors.full_messages
       erb :'users/new'
@@ -21,7 +21,7 @@ post '/users' do
 end
 
 #Users Show
-get 'users/:id' do
+get '/users/:id' do
   @user = User.find(params[:id])
   erb :'users/show'
 end
